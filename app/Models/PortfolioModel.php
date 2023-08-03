db545165f137581c51291c18d17ae9efc4a31385
AntonelloStefania <antonelloste94@gmail.com>
2023-08-03 19:53:49 +0200

    aggiunti faker alla tabella tramite seeder

--- app/Models/PortfolioModel.php
+++ app/Models/PortfolioModel.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class PortfolioModel extends Model
 {
     use HasFactory;
+    protected $fillable = ['title','front_ender','back_ender','ui','ux','illustrator','image','description','link'];
 }
--- database/seeders/PortfolioModelSeeder.php
+++ database/seeders/PortfolioModelSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\PortfolioModel;
+
+use Faker\Generator as Faker;
+use Faker\Provider\en_US\Person;
 
 class PortfolioModelSeeder extends Seeder
 {
@@ -12,8 +16,22 @@ class PortfolioModelSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i=1; $i<=10; $i++){
+            $site= new PortfolioModel();
+
+            $site->title= $faker->words(3, true);
+            $site->front_ender=$faker->name();
+            $site->back_ender=$faker->name();
+            $site->ui=$faker->name();
+            $site->ux=$faker->name();
+            $site->illustrator=$faker->name();
+            $site->link=$faker->url();
+            $site->description=$faker->paragraph();
+            $site->image=$faker->imageUrl(640, 480, 'animals', true);
+            
+            $site->save();
+        }
     }
 }
